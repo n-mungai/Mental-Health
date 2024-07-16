2b39f3194e0178d1fdfe94acafe1751e97967521
victorrndungu <131138431+victorrndungu@users.noreply.github.com>
2024-07-16 17:13:13 +0300

    User Page HTML and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LandingPage from './routes/landingPage/LandingPage'
 import Footer from './routes/Footer'
 import BlogsPage from './routes/blogsPage/BlogsPage'
 import ExpertsPage from './routes/expertsPage/ExpertsPage'
+import UserPage from './routes/userPage/UserPage'
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
       {
         path: "experts",
         element: <ExpertsPage />
+      },
+      {
+        path: "user",
+        element: <UserPage />
       }
     ]
   },
